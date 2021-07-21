10ff1b03e3084174c7cf09774a49b647930288b1
DevMeena <68868864+DevMeena@users.noreply.github.com>
2021-07-21 16:22:26 +0530

    added readme and removed toggle

---
+++ README.md
@@ -0,0 +1,8 @@
+#S1mple Adblocker
+
+S1mple Adblocker is a basic chrome extension which blocks advertisement from some adversisers.
+
+![adblocker ](images/adblocker-1.png)
+![adblocker ](images/adblocker-2.png)
+![adblocker ](images/after.png)
+![adblocker ](images/before.png)
--- background.js
+++ background.js
@@ -48,34 +48,34 @@ const adSites = [
 
 var enabled = true
 
-chrome.webRequest.onBeforeRequest.addListener(
+// chrome.webRequest.onBeforeRequest.addListener(
       
-	(details)=>{
-		if(!enabled) {
-		// 	console.log("false");
-		// 	return { cancel: false }
-		// }
-		// else {
-		// 	console.log("true");
-		// 	return { cancel: true }
-		// }
-		return { cancel: false };                           
-		}
-      var url = "https://www.google.com/";
-      return { redirectUrl: url };
-	},
-  	{urls: adSites},
- 	["blocking"]
-);
+// 	(details)=>{
+// 		if(!enabled) {
+// 		// 	console.log("false");
+// 		// 	return { cancel: false }
+// 		// }
+// 		// else {
+// 		// 	console.log("true");
+// 		// 	return { cancel: true }
+// 		// }
+// 		return { cancel: false };                           
+// 		}
+//       var url = "https://www.google.com/";
+//       return { redirectUrl: url };
+// 	},
+//   	{urls: adSites},
+//  	["blocking"]
+// );
 
 
 
 // auto toggle code
 
-// chrome.webRequest.onBeforeRequest.addListener(
-// 	()=>{
-// 		(details)=>{{cancel: true}}
-// 	},
-// 	{urls: adSites},
-// 	["blocking"]
-// )
+chrome.webRequest.onBeforeRequest.addListener(
+	()=>{
+		(details)=>{{cancel: true}}
+	},
+	{urls: adSites},
+	["blocking"]
+)
